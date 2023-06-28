41beab99969590308383bc14a3fe42afdb310892
Joao Mello <joaomello0016@gmail.com>
2023-06-28 20:01:00 -0300

    Adicionando métodos na controller

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -8,13 +8,27 @@ namespace FilmesAPI.Controllers
     public class FilmeController : ControllerBase
     {
         private static List<Filme> filmes = new List<Filme>();
+        private static int Id = 0;
 
         [HttpPost]
         public void AdicionarFilme([FromBody] Filme filme)
         {
+            filme.Id = Id++;
             filmes.Add(filme);
             Console.WriteLine(filme.Titulo);
             Console.WriteLine(filme.Duracao);
+        }
+
+        [HttpGet]
+        public IEnumerable<Filme> GetFilmes()
+        {
+            return filmes;
+        }
+
+        [HttpGet("{id}")]
+        public Filme? GetFilmeByYd(int id)
+        {
+            return filmes.FirstOrDefault(filme => filme.Id == id);
         }
     }
 }
